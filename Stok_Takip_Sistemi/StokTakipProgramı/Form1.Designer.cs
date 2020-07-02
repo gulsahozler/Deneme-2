76a2eaf022f0cf76f2bfbd6a7bd5cabe98c04c0e
Arzu Hebilli <arzuhbl98@gmail.com>
2020-07-03 02:52:40 +0300

    2.değişiklik

---
+++ Stok_Takip_Sistemi/StokTakipProgramı/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace StokTakipProgramı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.musteribilgisiBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.musteribilgileriveribaglantisiDataSet1 = new StokTakipProgramı.musteribilgileriveribaglantisiDataSet1();
+            this.musteribilgisiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.musteribilgileriveribaglantisiDataSet = new StokTakipProgramı.musteribilgileriveribaglantisiDataSet();
+            this.musteribilgisiTableAdapter = new StokTakipProgramı.musteribilgileriveribaglantisiDataSetTableAdapters.musteribilgisiTableAdapter();
+            this.musteribilgisiTableAdapter1 = new StokTakipProgramı.musteribilgileriveribaglantisiDataSet1TableAdapters.musteribilgisiTableAdapter();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgisiBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgileriveribaglantisiDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgisiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgileriveribaglantisiDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNo
+            // 
+            this.txtNo.Enabled = false;
+            this.txtNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNo.Location = new System.Drawing.Point(121, 59);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(89, 24);
+            this.txtNo.TabIndex = 0;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(121, 103);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(89, 24);
+            this.txtAd.TabIndex = 1;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTelefon.Location = new System.Drawing.Point(121, 147);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(89, 24);
+            this.txtTelefon.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Müşteri No:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(29, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Müşteri Adı:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(45, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Telefon:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(29, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Kayıt Tarihi:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(67, 274);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(79, 44);
+            this.btnKaydet.TabIndex = 8;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(382, 274);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(79, 44);
+            this.btnSil.TabIndex = 9;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGüncelle.Location = new System.Drawing.Point(585, 274);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(79, 44);
+            this.btnGüncelle.TabIndex = 10;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Silver;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(513, 187);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // musteribilgisiBindingSource1
+            // 
+            this.musteribilgisiBindingSource1.DataMember = "musteribilgisi";
+            this.musteribilgisiBindingSource1.DataSource = this.musteribilgileriveribaglantisiDataSet1;
+            // 
+            // musteribilgileriveribaglantisiDataSet1
+            // 
+            this.musteribilgileriveribaglantisiDataSet1.DataSetName = "musteribilgileriveribaglantisiDataSet1";
+            this.musteribilgileriveribaglantisiDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // musteribilgisiBindingSource
+            // 
+            this.musteribilgisiBindingSource.DataMember = "musteribilgisi";
+            this.musteribilgisiBindingSource.DataSource = this.musteribilgileriveribaglantisiDataSet;
+            // 
+            // musteribilgileriveribaglantisiDataSet
+            // 
+            this.musteribilgileriveribaglantisiDataSet.DataSetName = "musteribilgileriveribaglantisiDataSet";
+            this.musteribilgileriveribaglantisiDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // musteribilgisiTableAdapter
+            // 
+            this.musteribilgisiTableAdapter.ClearBeforeFill = true;
+            // 
+            // musteribilgisiTableAdapter1
+            // 
+            this.musteribilgisiTableAdapter1.ClearBeforeFill = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(121, 186);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(89, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.txtNo);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Müşteri Bilgileri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgisiBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgileriveribaglantisiDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgisiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.musteribilgileriveribaglantisiDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private musteribilgileriveribaglantisiDataSet musteribilgileriveribaglantisiDataSet;
+        private System.Windows.Forms.BindingSource musteribilgisiBindingSource;
+        private musteribilgileriveribaglantisiDataSetTableAdapters.musteribilgisiTableAdapter musteribilgisiTableAdapter;
+        private musteribilgileriveribaglantisiDataSet1 musteribilgileriveribaglantisiDataSet1;
+        private System.Windows.Forms.BindingSource musteribilgisiBindingSource1;
+        private musteribilgileriveribaglantisiDataSet1TableAdapters.musteribilgisiTableAdapter musteribilgisiTableAdapter1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
